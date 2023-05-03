a24be90fb3284fbd7436d661ec48a0db2bd170b1
Mokoyx <92005707+Mokoyx@users.noreply.github.com>
2023-05-03 22:19:21 +0800

    Add files via upload

---
+++ students/forms.py
@@ -0,0 +1,20 @@
+from django import forms 
+from .models import Student
+
+class StudentForm(forms.ModelForm):
+  class Meta:
+    model = Student
+    fields = ['student_number', 'first_name', 'last_name', 'field_of_study', 'gpa']
+    labels = {
+      'student_number': '#', 
+      'first_name': 'First Name', 
+      'field_of_study': 'Address', 
+      'gpa': 'Telephone'
+    }
+    widgets = {
+      'student_number': forms.NumberInput(attrs={'class': 'form-control'}), 
+      'first_name': forms.TextInput(attrs={'class': 'form-control'}),
+      'last_name': forms.TextInput(attrs={'class': 'form-control'}),
+      'field_of_study': forms.TextInput(attrs={'class': 'form-control'}),
+      'gpa': forms.NumberInput(attrs={'class': 'form-control'}),
+    }
